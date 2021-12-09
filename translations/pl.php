73005a4526cd8e445672b6777187e020bd207b3f
Patryk Tchorzewski <patryk@dark-side.pro>
2021-12-09 14:19:28 +0000

    add polish translations

---
+++ translations/pl.php
@@ -0,0 +1,6 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{dsfreeshippingfrom}prestashop>displayshoppingcart_23c320f434d4420bdbb433faeb1a6812'] = 'Wydaj następne';
+$_MODULE['<{dsfreeshippingfrom}prestashop>displayshoppingcart_dac22669af4470a5768d996a177f9f59'] = 'aby otrzymać przesyłkę za darmo!';
